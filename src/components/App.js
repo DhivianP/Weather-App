d54cbb60eba0417ab7199e09f269c9a2f84002f8
Dhivian Pirabaharan <dhivian1234@hotmail.co.uk>
2023-04-02 11:56:50 +0100

    LocationDetails and ForecastSummary

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import "../styles/App.css";
+import React from 'react';
+import LocationDetails from './LocationDetails';
+
+function App(props) {
+  const { location } = props;
+  return (
+    <div className="App">
+      <LocationDetails city={location.city} country={location.country} />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/tests/App.test.js
@@ -0,0 +1,12 @@
+import { render, screen } from "@testing-library/react";
+import React from "react";
+import App from "../components/App";
+import forecast from "../data/forecast.json";
+
+describe("App", () => {
+  it("renders App component", () => {
+    render(<App location={forecast.location} forecasts={forecast.forecasts} />);
+    const h1Element = screen.getByText(/Manchester, UK/i);
+    expect(h1Element).toBeInTheDocument();
+  });
+});
